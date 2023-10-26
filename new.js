4998146d2ef148d5b3445f496f71b6793be8b85d
ibadurrehman718 <ibadurrehman718@gmail.com>
2023-10-27 03:23:47 +0500

    Css improved

--- new.js
+++ new.js
@@ -2,15 +2,16 @@ const resultDiv = document.querySelector(".resultDiv");
 const data = JSON.parse(localStorage.getItem("news"));
 
 resultDiv.innerHTML = `
- <div class=" ">
+ <div class="m-2 ">
 <img class="m-1" width="80%" height="70%" src="${data.urlToImage}"  alt="...">
 <div class="card-body mt-8">
-<h1 class=" card-title text-3xl font-bold">${data.title}</h1>
-<p class="card-text">${data.content}</p>
-<p class="card-text">${data.description}</p>
-<h6 class="text-end card-title">Author : ${data.author}</h6>
-<h6 class="text-start card-title">Publisted at : ${data.publishedAt}</h6>
-<a href="${data.url}">Read Full Article at <span class="underline text-red-300">${data.url}</span></a>
+<h1 class=" card-title text-4xl font-bold">${data.title}</h1>
+<p class="card-text  mt-2 ">${data.content}</p>
+<p class="card-text mt-2 mb-2 font-medium">${data.description}</p>
+<a  href="${data.url}">Read Full Article at <span class="underline text-red-300">${data.url}</span></a>
+<span class= "flex justify-between mt-2"><h6 class=" inline-block card-title mt-2">Author : <span class="text-cyan-500">${data.author}</span></h6>
+<h6 class="card-title mt-2 inline-block ">Published at : ${data.publishedAt}</h6>
+</span>
 </div>
 <a href="./index.html" class=" mt-3 btn btn-primary">Go To Home</a>
 </div>`;
